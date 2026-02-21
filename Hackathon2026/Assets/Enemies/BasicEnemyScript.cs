debce9d0d5ca03ed3cda843d7b7917162f1cc9e0
Zachary Adelson <zachary.adelson@wsu.edu>
2026-02-21 14:41:42 -0800

    basic enemy tracking

--- Hackathon2026/Assets/Enemies/BasicEnemyScript.cs
+++ Hackathon2026/Assets/Enemies/BasicEnemyScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class BasicEnemyScript : MonoBehaviour
 {
+    public float speed;
+    public Transform player;
+ 
+    Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+        rigidBody.velocity = direction * speed;
+    }
+
     void Update()
     {
-        
+        Vector2 distanceToPlayer = player.position - transform.position;
+        if (distanceToPlayer.magnitude < 1.5f)
+        {
+            //insert code to damage player here
+            transform.GetComponent<SpriteRenderer>().color = Color.yellow; // Change enemy color to yellow when close
+        }
+        else
+        {
+            transform.GetComponent<SpriteRenderer>().color = Color.red; // Reset enemy color when not close
+        }
+
     }
 }
