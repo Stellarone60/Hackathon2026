33ca09066f4b25a58ad24188ff7199a4fdace85b
Stellarone60 <sarah.carley@hotmail.com>
2026-02-22 10:01:03 -0800

    trap spawn variety

--- Hackathon2026/Assets/Modifiers/AddTrapsRoomModifier.cs
+++ Hackathon2026/Assets/Modifiers/AddTrapsRoomModifier.cs
@@ -9,17 +9,24 @@ namespace Assets.Modifiers
 {
     public class AddTrapsRoomModifier : RoomModifier
     {
+        private int numberOfTrapsToAdd;
         public AddTrapsRoomModifier()
         {
             this.modifierName = "Add Traps";
             this.modifierSprite = Resources.Load<Sprite>("AddTrapsModifier");
-            this.description = $"Adds traps to the room.";
+            numberOfTrapsToAdd = UnityEngine.Random.Range(1, 4); // Randomly add between 1 and 3 traps.
+
+            this.description = $"Adds up to {numberOfTrapsToAdd} traps to the room.";
         }
         public override void ApplyModifier(RoomManager manager)
         {
             // Add a trap prop prefab to the room's prop prefabs.
-            GameObject trapPrefab = Resources.Load<GameObject>("testTrap");
-            manager.propPrefabs.Add(trapPrefab);
+            while (numberOfTrapsToAdd > 0)
+            {
+                GameObject trapPrefab = Resources.Load<GameObject>("testTrap");
+                manager.propPrefabs.Add(trapPrefab);
+                numberOfTrapsToAdd--;
+            }
         }
     }
 }
