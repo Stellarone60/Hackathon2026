715317b04ecdb93520687035fc5c9648e7b9750d
Stellarone60 <sarah.carley@hotmail.com>
2026-02-22 03:54:18 -0800

    evil char

--- Hackathon2026/Assets/Modifiers/RangedMeleeRatioRoomModifier.cs
+++ Hackathon2026/Assets/Modifiers/RangedMeleeRatioRoomModifier.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-\
+
 namespace Assets.Modifiers
 {
     public class RangedMeleeRatioRoomModifier : RoomModifier
